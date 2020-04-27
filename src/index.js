4f2e3657c264f7498763dae969ce7be575e16016
Arshad Feeroz <md.feeroz@myntra.com>
2020-04-27 15:55:21 +0530

    Added google analytics basic code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-import {hashHistory} from 'react-router'
+import { hashHistory } from "react-router";
+import ReactGA from "react-ga";
+import auth from "./auth.ts"; // Sample authentication provider
 
-require('./style.css')
+require("./style.css");
 
-require('setimmediate')
+require("setimmediate");
 
-var React = require('react')
-var {render} = require('react-dom')
-var Router = require('react-router/lib/Router')
-var useScroll = require('react-router-scroll/lib/useScroll')
-var applyRouterMiddleware = require('react-router/lib/applyRouterMiddleware')
+var React = require("react");
+var { render } = require("react-dom");
+var Router = require("react-router/lib/Router");
+var useScroll = require("react-router-scroll/lib/useScroll");
+var applyRouterMiddleware = require("react-router/lib/applyRouterMiddleware");
 
-var routes = require('./routes').default
+var routes = require("./routes").default;
+
+const trackingId = "UA-164641455-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.set({
+  userId: auth.currentUserId(),
+  // any data that is relevant to the user session
+  // that you would like to track with google analytics
+});
+ReactGA.pageview("/");
 
 render(
   <Router
@@ -18,5 +29,5 @@ render(
     render={applyRouterMiddleware(useScroll())}
     routes={routes}
   />,
-  document.getElementById('app')
-)
+  document.getElementById("app")
+);
